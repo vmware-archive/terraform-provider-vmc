353ac359397dfa84376385a7c46f9df7254a5786
saloneerege <srege@vmware.com>
2019-10-16 17:14:07 -0700

    Modified test file for public IP resource

--- vmc/resource_vmc_publicips_test.go
+++ vmc/resource_vmc_publicips_test.go
@@ -2,6 +2,7 @@ package vmc
 
 import (
 	"fmt"
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	"gitlab.eng.vmware.com/het/vmware-vmc-sdk/vapi/bindings/vmc/orgs/sddcs/publicips"
@@ -11,14 +12,15 @@ import (
 )
 
 func TestAccResourceVmcPublicIP_basic(t *testing.T) {
-
+	vmName := "terraform_test_vm_" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	vmList := []string {vmName}
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testCheckVmcSddcDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: testAccVmcPublicIPConfigBasic(),
+				Config: testAccVmcPublicIPConfigBasic(vmList),
 				Check: resource.ComposeTestCheckFunc(
 					testCheckVmcPublicIPExists("vmc_publicips.publicip_1"),
 				),
@@ -33,22 +35,29 @@ func testCheckVmcPublicIPExists(name string) resource.TestCheckFunc {
 		if !ok {
 			return fmt.Errorf("Not found: %s", name)
 		}
-		allocationID := rs.Primary.Attributes["id"]
+
 		sddcID := rs.Primary.Attributes["sddc_id"]
 		orgID := rs.Primary.Attributes["org_id"]
+		vmName := rs.Primary.Attributes["names"]
 		connector := testAccProvider.Meta().(client.Connector)
 		publicIPClient := publicips.NewPublicipsClientImpl(connector)
 
-		publicIP, err := publicIPClient.Get(orgID, sddcID, allocationID)
+		publicIPList , err := publicIPClient.List(orgID, sddcID)
+		if err != nil {
+			return fmt.Errorf("Bad: List on publicIP: %s", err)
+		}
+		allocationID := publicIPList[0].AllocationId
+		publicIP, err := publicIPClient.Get(orgID, sddcID, *allocationID)
 		if err != nil {
 			return fmt.Errorf("Bad: Get on publicIP: %s", err)
 		}
-
-		if *publicIP.AllocationId != allocationID {
-			return fmt.Errorf("Bad: Public IP %q does not exist", allocationID)
+        fmt.Println("Inside test for public IPs")
+		fmt.Println(vmName)
+		if *publicIP.Name != vmName {
+			return fmt.Errorf("Bad: Public IP %q does not exist", *allocationID)
 		}
 
-		fmt.Printf("Public IP created successfully with id %s ", allocationID)
+		fmt.Printf("Public IP created successfully with id %s ", *allocationID)
 		return nil
 	}
 }
@@ -79,7 +88,7 @@ func testCheckVmcSddcDestroy(s *terraform.State) error {
 	return nil
 }*/
 
-func testAccVmcPublicIPConfigBasic() string {
+func testAccVmcPublicIPConfigBasic(name []string) string {
 	return fmt.Sprintf(`
 provider "vmc" {
 	refresh_token = %q
@@ -90,17 +99,19 @@ provider "vmc" {
 }
 	
 data "vmc_org" "my_org" {
-	id = "05e0a625-3293-41bb-a01f-35e762781c2a"
+	id = %q
 }
 
 resource "vmc_publicips" "publicip_1" {
 	org_id = "${data.vmc_org.my_org.id}"
 	sddc_id = "4251fb8e-6fba-4880-aedb-ce3485873941"
-	names     = ["srege-test-VM-999"]
+	names     = %q
 	host_count = 1
 	private_ips = ["10.105.167.133"]
 }
 `,
 		os.Getenv("REFRESH_TOKEN"),
+		os.Getenv("ORG_ID"),
+		name,
 	)
 }
